b8f204690d9de54aa5aaf9a13a1f9df71ccd00bc
ihanseol <hanseol33@naver.com>
2019-12-31 12:36:17 +0900

    untar.fish modify

--- linux/fish_shell/untar.fish
+++ linux/fish_shell/untar.fish
@@ -1,17 +1,21 @@
-#!/usr/bin/fish
 function untar --description "Expand/extract tar files"
-    set --local ext (echo $argv[1] | awk -F. '{print $NF}')
+    set --local ext (echo $argv[1] | awk -F . '{print $NF}')
+    echo $ext, $argv[1]
     switch $ext
         case tar
-            tar -xvf $argv[1]
+            echo 'this is tar'
+            tar xvf $argv[1]
         case gz
-            tar -zxvf $arg[1]
+            echo 'this is gz'
+            tar -xvzf $argv[1]
         case bz2
-            tar -jxvf $argv[1]
+            echo 'this is bz2'
+            tar xvjf $argv[1]
         case '*'
             echo "unknown extension"
     end
 end
+
 
 
 
