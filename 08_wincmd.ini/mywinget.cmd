f8b2b10e857188c6fe3dd7f3f90a4039b53a0748
ihanseol <hanseol33@naver.com>
2023-07-02 23:01:36 +0900

    add

---
+++ 08_wincmd.ini/mywinget.cmd
@@ -0,0 +1,31 @@
+winget install -e --id Git.Git
+winget install -e --id Emurasoft.EmEditor
+winget install -e --id SublimeHQ.SublimeText.4
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id vim.vim
+winget install -e --id Notepad++.Notepad++
+
+winget install -e --id CodeSector.TeraCopy
+
+winget install -e --id Brave.Brave
+
+winget install -e --id Famatech.AdvancedIPScanner
+winget install -e --id KeePassXCTeam.KeePassXC
+
+winget install -e --id voidtools.Everything
+winget install -e --id Flow-Launcher.Flow-Launcher
+winget install -e --id VirusTotal.VirusTotalUploader
+
+
+winget install -e --id Notion.Notion
+
+winget install -e --id Microsoft.PowerToys
+winget install -e --id qBittorrent.qBittorrent
+
+winget install -e --id 7zip.7zip
+winget install -e --id RARLab.WinRAR
+winget install -e --id Microsoft.BingWallpaper
+
+winget install -e --id NGWIN.PicPick
+winget install -e --id Daum.PotPlayer
+winget install -e --id xiles.NexusFont
