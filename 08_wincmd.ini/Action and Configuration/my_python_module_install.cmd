a78848e7e1cf85c6252aee502df8f6be9c27fdd7
ihanseol <hanseol33@naver.com>
2024-07-05 13:54:58 +0900

    add

---
+++ 08_wincmd.ini/Action and Configuration/my_python_module_install.cmd
@@ -0,0 +1,51 @@
+echo 'Python Module Install ...'
+
+python -m pip install --upgrade pip
+
+pip install pyhwpx
+pip install hwpapi
+pip install pandas
+pip install openpyxl
+pip install xlwings
+pip install numpy
+pip install pillow
+pip install pdf2image
+pip install pyautogui
+pip install pywin32
+pip install -U pywinauto
+pip install pick
+pip install pyperclip
+pip install natsort
+pip install selenium
+pip install webdriver_manager
+pip install beautifulsoup4
+pip install pygetwindow
+pip install psutil
+pip install xlwings
+pip install natsort
+
+pip install opencv-python
+pip install pytesseract
+pip install -U matplotlib
+
+pip install screeninfo
+
+pip install playsound==1.2.2
+pip install thread6
+
+pip3 install pyside6
+pip install pyqt6
+pip install pyqt6-tools
+
+pip install pygubu
+pip install pygubu-designer
+
+pip install pymupdf
+pip install pynvim
+
+
+
+
+
+
+
